8147ce2ef4cc829f3f6140641fd98fd84ad5873c
LinaGSM <lina.gasmi@etu.univ-cotedazur.fr>
2025-01-25 18:52:51 +0100

    Training time added for gpu

--- pytorch_exercice/project/demo_gpu.py
+++ pytorch_exercice/project/demo_gpu.py
@@ -10,6 +10,8 @@ from torch.utils.data import DataLoader
 import os
 from torch.nn.parallel import DistributedDataParallel as DDP
 
+import csv
+
 
 def run(rank, size):
     torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
@@ -34,9 +36,49 @@ def run(rank, size):
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
 
+    with open('training_time_gpu.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        
+        # Ajouter les en-têtes si le fichier est vide
+        if file.tell() == 0:
+            writer.writerow(['Number of GPU','Loading Time', 'Computation + Communication Time', 'Real Time'])
+        
+        start = time.time()  # Temps de début total
+        
+        # Temps de chargement des données
+        start_loading_time = time.time()
+        train_images, train_labels = next(iter(dataloader))
+        train_images = train_images.to(device_id)
+        train_labels = train_labels.to(device_id)
+    
+    #    train_images, train_labels = next(iter(dataloader))
+        loading_time = time.time() - start_loading_time
+        print(f'Loading time: {loading_time} seconds')
+        
+        # Temps de calcul et de communication
+        start_comp_comm_time = time.time()
+        optimizer.zero_grad()
+        outputs = ddp_model(train_images)
+        loss_fn(outputs, train_labels).backward()
 
-    print(f"Start running basic DDP example on rank {rank} with model Resnet18.")
-    st = time.time()
+    #    optimizer.zero_grad()
+    #    outputs = ddp_model(train_images)
+    #    loss_fn(outputs, train_labels).backward()
+        computation_communication_time = time.time() - start_comp_comm_time
+        
+        real_time = time.time() - start  # Temps total réel
+        optimizer.step()
+        dist.destroy_process_group()
+        
+    #    optimizer.step()
+    #    dist.destroy_process_group()
+        print(f"Finished running basic DDP example on rank {rank}.")
+        print(f"Total time: {real_time} seconds")
+
+        # Écriture des données dans le fichier CSV
+        writer.writerow([size, loading_time, computation_communication_time, real_time])
+        print(f" Loading: {loading_time}s, Computation+Comm: {computation_communication_time}s, Total: {real_time}s")
+'''
     train_images, train_labels = next(iter(dataloader))
     train_images = train_images.to(device_id)
     train_labels = train_labels.to(device_id)
@@ -51,7 +93,7 @@ def run(rank, size):
     dist.destroy_process_group()
     print(f"Finished running basic DDP example on rank {rank}.")
     print(f"Total time: {et-st} seconds")
-
+'''
 if __name__ == "__main__":
     dist.init_process_group("nccl", init_method="env://")
     size = dist.get_world_size()
